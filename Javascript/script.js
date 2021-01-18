8e1fa815d0bfc1f483f6642b366a869d3a354148
rebeckhey <rebecka.pettersson.1993@gmail.com>
2021-01-18 19:41:03 +0100

    project

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,10 @@
-let users =[
+let users = [
     {
-        id:'1',
-        förNamn:'Förnamn',
-        efterNamn:'Efternamn',
-        epost:'email@example.com',
-        completed:false 
+        id: '1',
+        förNamn: 'Förnamn',
+        efterNamn: 'Efternamn',
+        epost: 'email@example.com',
+        completed: false
     }
 ]
 let form = document.querySelector('#formGroup');
@@ -13,15 +13,13 @@ let inputLastname = document.querySelector('#inputefternamn');
 let email = document.querySelector('#inputEmail4');
 let button = document.querySelector('#showList');
 let output = document.querySelector('#outputList');
-let edit = document.querySelector ('#editbtn');
-let trash = document.querySelector ('#trashbtn');
-
-
+let edit = document.querySelector('#editbtn');
+let trash = document.querySelector('#trashbtn');
 
 
 let lista = () => {
-    output.innerHTML=''
-    users.forEach (user => {   
+    output.innerHTML = ''
+    users.forEach(user => {
         output.innerHTML += `<div id="${user.id}"class="bg-white border rounded p-2 d-flex justify-content-between align-items-center mt-2"><p class="para mb-0" >${user.förNamn} ${user.efterNamn}<br><span>${user.epost}</span></p> 
         <div class="ml-5">
         <button id="editbtn" class="btn btn-dark"><i class="fas fa-edit"></i></button>
@@ -29,83 +27,61 @@ let lista = () => {
       </div>
       </div>`
     })
- 
 }
 users.shift(lista);
 
-
-button.addEventListener('click', (event) =>{
+button.addEventListener('click', (event) => {
     event.preventDefault()
-    if (inputFirstname.value !=='' && inputLastname.value !=='' && email.value !=='' && validEmail(email.value)){
-    let user = {
-        id: Date.now().toString(),
-        förNamn:inputFirstname.value,
-        efterNamn:inputLastname.value,
-        epost:email.value,
-        completed:false
-    }
-    users.push(user)
-    lista()
-    }
-    if (inputFirstname.value =='')
-    {
+    if (inputFirstname.value !== '' && inputLastname.value !== '' && email.value !== '' && validEmail(email.value)) {
+        errorFirstname.style.visibility = 'hidden'
+        errorLastname.style.visibility = 'hidden'
+        errorEmail.style.visibility = 'hidden'
+        let user = {
+            id: Date.now().toString(),
+            förNamn: inputFirstname.value,
+            efterNamn: inputLastname.value,
+            epost: email.value,
+        }
+        users.push(user)
+        lista()
+
+    } if (inputFirstname.value.length < 2) {
         errorFirstname.style.visibility = 'visible'
-        document.getElementById('errorFirstname').innerHTML=('Alla fält måste vara ifyllda!')
+        document.getElementById('errorFirstname').innerHTML = ('För kort namn. Minst 2 bokstäver!')
     }
-    if(inputLastname.value ==''){
+    if (inputLastname.value.length < 2) {
         errorLastname.style.visibility = 'visible'
-        document.getElementById('errorLastname').innerHTML=('Alla fält måste vara ifyllda!')
-        
+        document.getElementById('errorLastname').innerHTML = ('För kort namn. Minst 2 bokstäver!')
     }
-    if(email.value ==''){
+    if (inputFirstname.value == '') {
+        errorFirstname.style.visibility = 'visible'
+        document.getElementById('errorFirstname').innerHTML = ('Alla fält måste vara ifyllda!')
+    }
+    if (inputLastname.value == '') {
+        errorLastname.style.visibility = 'visible'
+        document.getElementById('errorLastname').innerHTML = ('Alla fält måste vara ifyllda!')
+
+    }
+    if (!validEmail(email.value)) {
         errorEmail.style.visibility = 'visible'
-        document.getElementById('errorEmail').innerHTML=('Alla fält måste vara ifyllda!')
+        document.getElementById('errorEmail').innerHTML = ('Tänk på att din email måste innehålla @ och får inte innehålla åäö!')
     }
-    else if (!validEmail (email.value)){
+    if (email.value == '') {
         errorEmail.style.visibility = 'visible'
-        document.getElementById('errorEmail').innerHTML=('Tänk på att din email måste innehålla @ och får inte innehålla åäö!')
+        document.getElementById('errorEmail').innerHTML = ('Alla fält måste vara ifyllda!')
     }
-    function validEmail (email){
+
+    function validEmail(email) {
         return /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i.test(email)
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// function validateemail()  
-// {  
-// let atposition=email.value.indexOf("@");  
-// let dotposition=email.value.lastIndexOf(".");  
-// if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-//   alert("Ange en giltig emailadress");  
-//   return false;  
-//   }  
-// }  
-
-
-
-// function valid_email(email) {
-//     return email.match(/^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i);
-//  }
-// valid_email()
-
-
-
-
-
-
-
-
-
-let testing = /\\S+@\\S+/
-
-
+trash.addEventListener('click', (e) => {
+    e.preventDefault()
+    lista()
+})
+output.addEventListener ('click', (e) => {
+    e.preventDefault()
+    users = users.filter(user => user.id !== e.target.parentNode.id)
+    lista();
+    console.log('')
+  })
